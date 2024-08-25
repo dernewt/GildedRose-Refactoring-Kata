552e7875078466d0b1880f9fe0747393acab48ab
dernewt <dernewt@gmail.com>
2024-08-25 18:40:36 -0400

    first pass with furofo, mostly focused on the program and test structure, didnt touch the meat of the problem yet.

---
+++ csharp.xUnit/GildedRose/ProgramHelper.cs
@@ -0,0 +1,14 @@
+﻿using System.Linq;
+
+namespace GildedRoseKata;
+
+class ProgramHelper
+{
+    public static int ParseArgs(string[] args)
+    {
+        if (int.TryParse(args.FirstOrDefault(), out var days))
+            return days;
+
+        return 1;
+    }
+}
---
+++ csharp.xUnit/GildedRose/Services/Logger.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace GildedRoseKata.Services;
+
+public class Logger
+{
+    public static void LogDay(int day)
+    {
+        Console.WriteLine($"-------- day {day} --------");
+    }
+
+    public static void LogItems(List<Item> items)
+    {
+        Console.WriteLine("name, sellIn, quality");
+        for (var j = 0; j < items.Count; j++)
+        {
+            Console.WriteLine(items[j].Name + ", " + items[j].SellIn + ", " + items[j].Quality);
+        }
+        Console.WriteLine("");
+    }
+
+    public static void LogStart()
+    {
+        Console.WriteLine("OMGHAI!");
+    }
+}
