b5bb46482220f5b9e308e43a0d377cb6fb703b43
furofo <chriscelllphonerepair@gmail.com>
2024-08-31 22:47:34 +0300

    2nd pass refactored gilded rose to remove most of nesting if statements into methods for better semantics

--- csharp.xUnit/GildedRose/GildedRose.cs
+++ csharp.xUnit/GildedRose/GildedRose.cs
@@ -1,4 +1,8 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.Design;
+using System.Linq;
+using System.Runtime.CompilerServices;
 
 namespace GildedRoseKata;
 
@@ -11,45 +15,53 @@ public class GildedRose
         this.Items = Items;
     }
 
+    public string[] specialItems = { "Aged Brie", "Backstage passes to a TAFKAL80ETC concert", "Sulfuras, Hand of Ragnaros" };
+
+    public bool IsSpecialItem(string itemName)
+    {
+        return specialItems.Contains(itemName);
+    }
+
+    public void IncreaseQuantity(Item item)
+    {
+        if (item.Quality < 50)
+        {
+            item.Quality = item.Quality + 1;
+
+            if (item.Name == "Backstage passes to a TAFKAL80ETC concert")
+            {
+                if (item.SellIn < 11)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+
+                if (item.SellIn < 6)
+                {
+                    if (item.Quality < 50)
+                    {
+                        item.Quality = item.Quality + 1;
+                    }
+                }
+            }
+
+        }
+    }
+
+
     public void UpdateQuality()
     {
         for (var i = 0; i < Items.Count; i++)
         {
-            if (Items[i].Name != "Aged Brie" && Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
+            if (Items[i].Quality > 0 && !IsSpecialItem(Items[i].Name) )
             {
-                if (Items[i].Quality > 0)
-                {
-                    if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                    {
-                        Items[i].Quality = Items[i].Quality - 1;
-                    }
-                }
+            Items[i].Quality = Items[i].Quality - 1;
             }
             else
             {
-                if (Items[i].Quality < 50)
-                {
-                    Items[i].Quality = Items[i].Quality + 1;
-
-                    if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].SellIn < 11)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-
-                        if (Items[i].SellIn < 6)
-                        {
-                            if (Items[i].Quality < 50)
-                            {
-                                Items[i].Quality = Items[i].Quality + 1;
-                            }
-                        }
-                    }
-                }
+                IncreaseQuantity(Items[i]);
             }
 
             if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
@@ -59,29 +71,16 @@ public class GildedRose
 
             if (Items[i].SellIn < 0)
             {
-                if (Items[i].Name != "Aged Brie")
-                {
-                    if (Items[i].Name != "Backstage passes to a TAFKAL80ETC concert")
-                    {
-                        if (Items[i].Quality > 0)
-                        {
-                            if (Items[i].Name != "Sulfuras, Hand of Ragnaros")
-                            {
-                                Items[i].Quality = Items[i].Quality - 1;
-                            }
-                        }
-                    }
-                    else
-                    {
-                        Items[i].Quality = Items[i].Quality - Items[i].Quality;
-                    }
+                if (Items[i].Name == "Aged Brie" && Items[i].Quality < 50) {
+                    Items[i].Quality = Items[i].Quality + 1;
                 }
-                else
-                {
-                    if (Items[i].Quality < 50)
-                    {
-                        Items[i].Quality = Items[i].Quality + 1;
-                    }
+               else if (Items[i].Name == "Backstage passes to a TAFKAL80ETC concert") {
+                    Items[i].Quality = Items[i].Quality - Items[i].Quality;
+                }
+
+                else if (Items[i].Quality > 0 && !IsSpecialItem(Items[i].Name))
+                {   
+                    Items[i].Quality = Items[i].Quality - 1;
                 }
             }
         }
--- csharp.xUnit/GildedRoseTests/ApprovalTest.cs
+++ csharp.xUnit/GildedRoseTests/ApprovalTest.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using VerifyXunit;
 using Xunit;
 using GildedRoseKata;
+using System.Collections.Generic;
 
 namespace GildedRoseTests;
 
@@ -25,5 +26,25 @@ public class ApprovalTest
         var output = fakeoutput.ToString();
 
         return Verifier.Verify(output);
+    }
+    [Fact]
+    public void IsSpecialItem_ShouldReturnFalseForNonSpecialItems()
+    {
+        // Arrange
+        var gildedRose = new GildedRose(new List<Item>());
+
+        // Act & Assert
+        Assert.False(gildedRose.IsSpecialItem("Normal Item"));
+        Assert.False(gildedRose.IsSpecialItem("Conjured Mana Cake"));
+    }
+    [Fact]
+    public void IsSpecialItem_ShouldReturnTrueForSpecialItems()
+    {
+        var gildedRose = new GildedRose(new List<Item>());
+        // Act & Assert
+        Assert.True(gildedRose.IsSpecialItem("Aged Brie"));
+        Assert.True(gildedRose.IsSpecialItem("Backstage passes to a TAFKAL80ETC concert"));
+        Assert.True(gildedRose.IsSpecialItem("Sulfuras, Hand of Ragnaros"));
+
     }
 }
